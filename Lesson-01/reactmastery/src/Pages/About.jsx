6d2e12c58a26805758c96bfa08cd70f9d63e8818
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-09-27 16:11:28 +0600

    lip8

--- Lesson-01/reactmastery/src/Pages/About.jsx
+++ Lesson-01/reactmastery/src/Pages/About.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
 
 class About extends Component {
+  state = {
+    name: "Ahsanul Mostakin",
+    age: 25,
+    Prof: "Content Creator",
+  };
   render() {
     return (
       <>
-        <h1>This is About Page</h1>
+        <h1>
+          {"My name is " +
+            this.state.name +
+            ", My profession is " +
+            this.state.Prof}
+        </h1>
       </>
     );
   }
 }
+
 export default About;
--- Lesson-01/reactmastery/src/Pages/Home.jsx
+++ Lesson-01/reactmastery/src/Pages/Home.jsx
@@ -3,15 +3,6 @@ import Contact from "./Contact";
 
 const AboutMe = "My Name Is Ahsanul Mostakin.";
 function Home() {
-  return (
-    <div>
-      <Contact name="Kuddus Miya" number="01814202161" />
-      <Contact name="Kuddus Chele" number="0181****" />
-      <Contact name="Abbas" number="0123456" />
-      <Contact name="Abbas x " number="0123456" />
-      <Contact name="Abbas y" number="0123456" />
-      <Contact name="Abbas z" number="0123456" />
-    </div>
-  );
+  return <div></div>;
 }
 export default Home;
