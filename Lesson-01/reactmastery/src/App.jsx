08b356a57273caa979c003f0f8378ace0740982c
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-09-27 14:30:24 +0600

    lip5

--- Lesson-01/reactmastery/src/App.jsx
+++ Lesson-01/reactmastery/src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
 import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Feedback from "./Pages/Feedback";
 function App() {
   return (
     <div className="App">
       <Home />
+      <About />
+      <Contact />
+      <Feedback />
     </div>
   );
 }
--- Lesson-01/reactmastery/src/Pages/Home.jsx
+++ Lesson-01/reactmastery/src/Pages/Home.jsx
@@ -3,10 +3,9 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
+      <h1>Header</h1>
+      <h1>Sidebar</h1>
+      <h1>Footer</h1>
     </div>
   );
 }
