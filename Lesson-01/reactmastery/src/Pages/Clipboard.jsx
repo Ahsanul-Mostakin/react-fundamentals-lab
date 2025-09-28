6b281035a5a4f0bd81744dc4a423091743d4df6c
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-09-28 06:48:12 +0600

    lip

--- Lesson-01/reactmastery/src/Pages/Clipboard.jsx
+++ Lesson-01/reactmastery/src/Pages/Clipboard.jsx
@@ -1,9 +1,32 @@
 import React from "react";
 
 const Clipboard = () => {
+  const CopyText = () => {
+    alert("Text Copied");
+  };
+
+  const OnCutText = () => {
+    console.log("Text Cuted Successful");
+  };
+
+  const OnPasteText = () => {
+    document.write("Text Paste Done");
+  };
+
   return (
     <div>
-      <p></p>
+      <p onCopy={CopyText}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam, hic.
+      </p>
+      <h1 onCut={OnCutText}>
+        This is Clipboard page you have cut some text from here
+      </h1>
+
+      <input
+        type="text"
+        onPaste={OnPasteText}
+        placeholder="Put your text here"
+      />
     </div>
   );
 };
