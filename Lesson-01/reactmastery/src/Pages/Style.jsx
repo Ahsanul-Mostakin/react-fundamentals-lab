02aff8fda493e25cf897c8abcab8a7e614db2613
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-09-28 18:36:58 +0600

    lip17

--- Lesson-01/reactmastery/src/Pages/Style.jsx
+++ Lesson-01/reactmastery/src/Pages/Style.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-
+import "./Style.css";
 function Style() {
   const ElementOne = {
-    color: "red",
+    color: "white",
     background: "green",
     textAlign: "center",
   };
   return (
     <div>
       <h1 style={ElementOne}>My first style components</h1>
+      <h2 className="programmer">Mostakin </h2>
     </div>
   );
 }
