ee0c255855c60627655b35fa3b6885184dda570b
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-10-06 18:11:38 +0600

    lip

---
+++ react-state-class/src/Counter.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export default function Counter() {
+  let [count, setCount] = useState(10);
+
+  function incCount() {
+    count += 1;
+    setCount(count);
+  }
+  return (
+    <div>
+      <h1>Count={count}</h1>
+      <button onClick={incCount}>Increase Count</button>
+    </div>
+  );
+}
