aa5d8f5c4ac69f08b5a43f35aa4f57f73835434e
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-10-07 19:33:20 +0600

    lip8m

--- react-state-class/src/Lottery.jsx
+++ react-state-class/src/Lottery.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+import "./Lottery.css";
+
 export default function Lottery() {
+  const [ticket, setTicket] = useState([0, 0, 0]);
+
   return (
     <div>
       <h1>Lottery Game</h1>
+      <div>
+        <span>{ticket[0]}</span>
+        <span>{ticket[1]}</span>
+        <span>{ticket[2]}</span>
+      </div>
     </div>
   );
 }
