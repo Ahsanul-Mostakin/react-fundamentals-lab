a64c26041833100466bc99e230c53933ba156bc0
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-10-08 12:36:05 +0600

    lip

--- react-state-class/src/Lottery.jsx
+++ react-state-class/src/Lottery.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import "./Lottery.css";
-import { genTicket } from "../helper";
+import { genTicket, sum } from "../helper";
 
 export default function Lottery() {
   const [ticket, setTicket] = useState(genTicket(3));
   let isWinning = sum(ticket) == 15;
+  let buyTicket = () => {
+    setTicket(genTicket(3));
+  };
   return (
     <div className="h1">
       <h1>Lottery Game</h1>
@@ -13,6 +16,10 @@ export default function Lottery() {
         <span>{ticket[1]}</span>
         <span>{ticket[2]}</span>
       </div>
+      <button className="btn" onClick={buyTicket}>
+        Buy new ticket
+      </button>
+      <h1>{isWinning && "Congratualation, You won"}</h1>
     </div>
   );
 }
